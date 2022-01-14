4c8e28f47903b93bdb7051a9b90ba6f685bcfe92
Davi Marques <davimgcb@gmail.com>
2022-01-14 15:30:25 -0300

    rota para pegar os dados do usuario na tela de profile

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -7,19 +7,30 @@ import { Context } from '../Context/AuthContext';
 import './../styles/button.css'
 
 export default function Profile() {
-  // const emailStorage = localStorage.getItem('email');
+
+  const [data, setData] = useState({})
+
+  const emailStorage = localStorage.getItem('email');
+  const id = localStorage.getItem('id');
+
+  const email = emailStorage.replace(/['"]+/g, '')
 
   const { handleLogout } = useContext(Context);
   
-  // useEffect(() => {
-  //   (async () => {
-  //     const { data } = await api.get('/rotaDoProfile');
-  //   })();
-  // }, []);
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get(`/user/${email}`);
+      console.log('datinha', data)
+    })();
+  }, []);
+
 
   return(
     <>
-    <h1>lal</h1>
+    {
+      console.log( 'data', data)
+    }
+    <h1>lal {email}</h1>
     <button onClick={()=> handleLogout()}>sair</button>
     </>
   )
