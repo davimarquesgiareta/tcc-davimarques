d4e6aa22dd58b7d1cf9765034ed2d2a15301f987
Davi Marques <davimgcb@gmail.com>
2022-01-15 16:37:46 -0300

    componetizando a pagina do perfil

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -1,7 +1,57 @@
 import React from "react"
+import { SocialIcon } from 'react-social-icons';
 
-export default function Profile (){
+export default function MyProfile (props){
+  const {user } = props
+
   return(
-    <h3>Profile</h3>
+    <div id="content">
+
+                    
+
+            <h2>Meu Perfil </h2>
+            <p> <strong>Nome:</strong> {user.name} </p>
+            <p> <strong>Cidade:</strong> {user.city} </p>
+            <p> <strong>Posição Política:</strong> {user.politic} </p>
+            <p> <strong>Religiosidade:</strong> {user.religiosity} </p>
+            <p> <strong>Minhas Tags: </strong> 
+            {user.tags + ''}
+            </p>
+
+            
+
+            <div className='row' >
+             <div style={{width:"100%", display:"flex", marginLeft:"15px"}}>
+               
+              { /* Tem que ter o https se não da ruim */}
+              {/* { user.socialMedias && <SocialIcon  url={`https://${user.socialMedias[1]}`}/>   } */}
+              
+              <div style={{marginRight: "10px"}}>
+                <SocialIcon  url={user.instagramLink}/> 
+              </div>
+
+              { user.socialMedias && 
+                <>
+                  {
+                    user.socialMedias[0] !== '' ? 
+                    <div style={{marginRight: "10px"}} >
+                    <SocialIcon  url={user.socialMedias[0]}/>  
+                    </div>
+                    : ''
+                  }
+                  {
+                    user.socialMedias[1] !== '' ? <SocialIcon  url={user.socialMedias[1]}/>  : ''
+                  }
+                </>   
+              }
+              </div>
+            </div>
+
+            <a href={`https://api.whatsapp.com/send?phone=55 ${user.whatsapp}&text=Olá, vi seu perfil no Influencers Search, e gostaria fazer uma parceria!`} class="float" target="_blank">
+            <i  class="fa fa-whatsapp my-float"></i>
+            </a>
+
+            <div class="line"></div>            
+        </div>
   )
 }
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -6,6 +6,8 @@ import { Context } from '../Context/AuthContext';
 import NavBar from '../Components/NavBar';
 import ReactWhatsapp from 'react-whatsapp';
 import { SocialMediaIconsReact } from 'social-media-icons-react';
+import { SocialIcon } from 'react-social-icons';
+import MyProfile from '../Components/Profile';
 
 import "../styles/Profile.css"
 
@@ -19,6 +21,8 @@ export default function Profile() {
   const emailStorage = localStorage.getItem('email');
   const id = localStorage.getItem('id');
 
+  var link = "twitter.com/davizerashow"
+
   const email = emailStorage.replace(/['"]+/g, '')
 
   const { handleLogout } = useContext(Context);
@@ -31,56 +35,18 @@ export default function Profile() {
     })();
   }, []);
 
+ 
+  
+  // console.log('ai', typeof(user.socialMedias) )
+
 
   return(
     <>
     <div class="wrapper">
        
         <NavBar user={user}/>
-
-        <div id="content">
-
-                    
-
-            <h2>Meu Perfil </h2>
-            <p> <strong>Nome:</strong> {user.name} </p>
-            <p> <strong>Cidade:</strong> {user.city} </p>
-            <p> <strong>Posição Política:</strong> {user.politic} </p>
-            <p> <strong>Religiosidade:</strong> {user.religiosity} </p>
-            <p> <strong>Minhas Tags: </strong> 
-            {user.tags + ''}
-            </p>
-
-            <div className='row' >
-             <div className='col-1'>
-               {
-                 console.log(user.socialMedias)
-               }
-               {
-                 
-                 user.socialMedias && <h2> to existindo</h2>
-               }
-               
-             <SocialMediaIconsReact  borderColor="rgba(0,0,0,0.25)" icon="twitter" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(26,166,233,1)" url="https://some-website.com/my-social-media-url" size="48" />
-             </div>
-             <div className='col-5'>
-             <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" icon="youtube" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(255,36,0,1)" url="https://some-website.com/my-social-media-url" size="48" />
-             </div>
-            </div>
-            
-            <div style={{background:"yellow" }}>
-            
-            
-            
-            </div>
-            
-                      
-            <a href={`https://api.whatsapp.com/send?phone=55 ${user.whatsapp}&text=Olá, vi seu perfil no Influencers Search, e gostaria fazer uma parceria!`} class="float" target="_blank">
-            <i  class="fa fa-whatsapp my-float"></i>
-            </a>
-
-            <div class="line"></div>            
-        </div>
+        <MyProfile user={user} />
+        
     </div>
     </>
   )
